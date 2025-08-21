9175df5485726529715301369ef787ec6f61de55
Nabila Trippe <nabilatrippe@gmail.com>
2025-08-21 13:13:08 -0300

    Avance en el TP2

--- TP2/testAPI.js
+++ TP2/testAPI.js
@@ -114,16 +114,82 @@ returnProductById();
 
 //Eliminar un producto (DELETE)
 
+async function deleteProduct(id) {
+  try {
+    const response = await fetch(`${BASE_URL}/products/${id}`, {
+      method: 'DELETE'
+    });
 
+    // Procesamos la respuesta de la API
+    const data = await response.json();
+    console.log("Respuesta de la API:", data);
 
-
-//FUNCION PARA QUE MUESTRE EN ORDEN LAS CONSIGNAS
-/*async function main() {
-  await getAllProducts();     // espera a que termine
-  await getLimitedProducts(10); // recién después hace esto
-  await persistFile();
-  await postNewProduct();
-  await returnProductById();
+    console.log(`Producto con id ${id} eliminado`);
+  } catch (error) {
+    console.error("Error al eliminar el producto:", error);
+  }
 }
 
-main();*/
+deleteProduct(5);  //se pasa por parametro el numero de id del producto que queremos eliminar
+
+
+//Modificar los datos de un producto (UPDATE)
+
+async function updateProduct(id, newData){
+  try {
+    const response = await fetch(`${BASE_URL}/products/${id}`, {
+      method: 'PUT', //para modificar la totalidad del producto, si el cambio es parcial debemos usar PATCH
+      headers: {
+        "Content-Type": "application/json", //  se aclara que enviamos un JSON
+      },
+      body: JSON.stringify(newData),
+    });
+    
+    const data = await response.json();
+    console.log("Producto actualizado (simulado):", data);
+  
+  } catch (error){
+      console.error("Error al modificar los datos del producto", error);
+  }
+}
+async function putNewProduct() {
+  const nuevoProducto = {
+   "id": 32,
+    "title": "Zapatillas deportivas ",
+    "price": 83,
+    "description": "Zapatillas negras del 34 al 44",
+    "category": "zapatillas",
+    "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+    "rating": {
+      "rate": 4.1,
+      "count": 259
+        }
+  };
+  
+
+  await updateProduct(32, nuevoProducto);
+}
+
+putNewProduct(); 
+
+
+//Agregar producto al archivo local
+
+async function addProductToLocalFile() {
+  const nuevoProducto = {
+   "id": 67,
+    "title": "Zapatillas  ",
+    "price": 83,
+    "description": "Zapatillas negras",
+    "category": "zapatillas",
+    "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+    "rating": {
+      "rate": 4.1,
+      "count": 259
+        }
+  };    
+  await saveToFile(nuevoProducto, "./productos.json"); // lo guardamos en archivo local, reutilizando funcion saveToFile
+  console.log ("Este es el producto agregado a tu archivo local:", nuevoProducto.title )
+}
+
+addProductToLocalFile();
