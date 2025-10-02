87315a2806cfbc64c755594ce82e82c3e846a05b
Franco Ñaña <franconana3@gmail.com>
2025-10-02 17:07:04 -0300

    Actualización 2

--- Trabajo Practico Final Integrador/reservas.js
+++ Trabajo Practico Final Integrador/reservas.js
@@ -6,31 +6,46 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { conexion } from './src/db/conexion.js';
 
-
 const router = express.Router();
 
-router.post('/', async (req, res) => { // POST http://localhost:3000/api/v1/notificacion
+router.post('/', async (req, res) => {
   const { fecha, salon, turno, correoDestino } = req.body;
   if (!fecha || !salon || !turno || !correoDestino)
     return res.status(400).json({ ok: false, mensaje: 'ERROR: Faltan Datos Obligatorios.' });
 
   try {
     const [salonExiste] = await conexion.query('SELECT * FROM salones WHERE titulo=?', [salon]);
-    if (salonExiste.length === 0) return res.status(404).json({ ok: false, mensaje: 'ERROR: El Salón No Pudo Ser Encontrado.' });
+    if (salonExiste.length === 0)
+      return res.status(404).json({ ok: false, mensaje: 'ERROR: El Salón No Pudo Ser Encontrado.' });
 
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     const plantillaHbs = path.join(__dirname, './src/utiles/handlebars/plantilla.hbs');
     const hbsContent = await readFile(plantillaHbs, 'utf-8');
-    const html = handlebars.compile(hbsContent)({ fecha, salon, turno });
+
+    const html = handlebars.compile(hbsContent)({
+      fecha,
+      salon,
+      turno,
+      url_ver_reserva: `http://localhost:3000/api/v1/salones/${salon}`
+    });
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
-      auth: { user: process.env.USER, pass: process.env.PASS }
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS
+      }
     });
 
-    await transporter.sendMail({ to: correoDestino, subject: 'Notificación de reserva', html });
-    res.json({ ok: true, mensaje: 'Correo Enviado Con Exito.' });
+    await transporter.sendMail({
+      to: correoDestino,
+      subject: 'Notificación de Reserva - GrupoAO',
+      html
+    });
+
+    console.log(`Reserva Enviada a ${correoDestino} Para el Salón ${salon} el Día ${fecha} en Turno ${turno}.`);
+    res.json({ ok: true, mensaje: 'Correo Enviado Con Éxito.' });
 
   } catch (err) {
     console.error(err);
